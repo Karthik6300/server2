fe53c5aed6967879f1c0effa7e3a0108859e4d44
Nayani_Karthik <nayanikarthik961@gmail.com>
2025-02-21 02:31:07 +0530

    server create

--- server.js
+++ server.js
@@ -1,10 +1,37 @@
-const http =require("http");
-const server = http.createServer((req,res)=>{
-    res.write("Hello World");
-    res.end("Response provided");
+const http = require("http");
+const url = require("url");
+let obj = { name: "karthik", city: "hyd" };
+const server = http.createServer((req, res) => {
+  //   console.log(req.method)
+  //   console.log(req.url)
+  const pursedurl = url.parse(req.url,true);
+  const tickets= pursedurl.query.tickets;
+  console.log(pursedurl)
 
+  if (req.method == "GET") {
+    if (pursedurl.pathname == "/bahubali") {
+      res.writeHead(200, "okay", { "content-type": "application/json" });
+      res.write(JSON.stringify({ "movie name": "bahubali", screen: "1" , numoftickets: tickets }));
+      res.end();
+    } else if (pursedurl.pathname == "/baby") {
+      res.writeHead(200, "okay", { "content-type": "application/json" });
+      res.write(JSON.stringify({ "movie name": "baby", screen: "2",numoftickets: tickets }));
+      res.end();
+    } else if (pursedurl.pathname == "/baba") {
+      res.writeHead(200, "okay", { "content-type": "application/json" });
+      res.write(JSON.stringify({ "movie name": "baba", screen: "3",numoftickets: tickets }));
+      res.end();
+    } else {
+      res.writeHead(402, "file not found",{"content-type":"application/json"});
+      res.write(JSON.stringify({ "movie name": "N/A", screen: "N/A" }));
+      res.end();
+    }
+  } else {
+    res.writeHead(402, "file not found");
+    res.end("invalid request");
+  }
 });
 
-server.listen("3101",()=>{
-    console.log("server running")
-})
+server.listen("3001", () => {
+  console.log("server running");
+});
